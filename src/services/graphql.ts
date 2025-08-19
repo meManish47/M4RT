7b77ad5cedf3715d323995d9022fa474c1f66a6b
Manish Kumar <kmanish57610@gmail.com>
2025-08-19 12:04:02 +0530

    Deployemnet part 2

--- src/services/graphql.ts
+++ src/services/graphql.ts
@@ -1,4 +1,4 @@
 import { GraphQLClient } from "graphql-request";
 export const gqlClient = new GraphQLClient(
-  `${process.env.NEXT_PUBLIC_API_URL || "/api/graphql"}`
+  `${process.env.NEXT_PUBLIC_API_URL}/api/graphql`
 );
